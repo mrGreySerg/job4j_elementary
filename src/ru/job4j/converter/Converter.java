0068e80636f86d2edb223eac86ed99b45c53b686
sergei kustrin <sergei@mail.ru>
2020-04-03 14:55:50 +0300

    5. Конвертер валюты[#264981]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j.converter;
+
+public class Converter{
+
+    public static int rubleToEuro(int value){
+        int rs1 = value / 70;
+        return rs1;
+    }
+
+    public static int rubleToDollar(int value){
+        int rs1 = value / 60;
+        return rs1;
+    }
+
+    public static void main(String[] args){
+        int euro = Converter.rubleToEuro(100);
+        int dollar = Converter.rubleToDollar(100);
+        System.out.println("100 rubles are " + euro + " euro.");
+        System.out.println("100 rubles are " + dollar + " dollar.");
+    }
+}
