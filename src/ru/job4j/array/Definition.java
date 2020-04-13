8fb61dba23ce607d5f9fa32be95311153e814e1d
sergei kustrin <sergei@mail.ru>
2020-04-13 16:19:49 +0300

    6.0.1. Объявление массива.[#264978]

---
+++ src/ru/job4j/array/Definition.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * Создание массивов.
+ * @author Kustrin Sergei.
+ * @version 1
+ */
+public class Definition {
+
+    /**
+     * Method main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
