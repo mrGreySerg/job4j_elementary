473ddffe86e8b700399f648eacdf4d9defcb8fb6
sergei kustrin <sergei@mail.ru>
2020-04-14 12:37:02 +0300

    6.2. Упорядочить массив.[#264979]

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+/**
+ * Class AlgoArray sorts the array in ascending order.
+ * @author Kustrin Sergei.
+ * @version 1.
+ */
+public class AlgoArray {
+
+    /**
+     * Method main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
+
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
