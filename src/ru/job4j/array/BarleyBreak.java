d4fe5327b5a6b836633c6a92920998639ff39e10
sergei kustrin <sergei@mail.ru>
2020-04-16 15:44:30 +0300

    6.6.1. Двухмерный массив. Вывод на консоль.[#264976]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * class BarleyBreak creates a two-dimensional array.
+ * @author Kustrin Sergei.
+ * @version 1.
+ */
+public class BarleyBreak {
+
+    /**
+     * method main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        int[][] myArray = new int[3][3];
+        myArray[0][0] = 1;
+        myArray[0][1] = 2;
+        myArray[0][2] = 3;
+        myArray[1][0] = 4;
+        myArray[1][1] = 5;
+        myArray[1][2] = 6;
+        myArray[2][0] = 7;
+        myArray[2][1] = 8;
+        myArray[2][2] = 9;
+    }
+}
