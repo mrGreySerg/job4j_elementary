a1fba4dd5d6199576598b95ddf493ee722ef4356
sergei kustrin <sergei@mail.ru>
2020-04-28 08:35:18 +0300

    1. Конструктор по умолчанию.[#265123]

---
+++ src/ru/job4j/animal/Animal.java
@@ -0,0 +1,13 @@
+package ru.job4j.animal;
+
+public class Animal {
+
+    public Animal() {
+        super();
+        System.out.println("class Animal!!!");
+    }
+
+    public static void main(String[] args) {
+        Tiger angry = new Tiger();
+    }
+}
---
+++ src/ru/job4j/animal/Predator.java
@@ -0,0 +1,9 @@
+package ru.job4j.animal;
+
+public class Predator extends Animal {
+
+    public Predator() {
+        super();
+        System.out.println("class Predator!!!");
+    }
+}
