30d9090bd7616b970fc1e9fca237fda3b8805803
sergei kustrin <sergei@mail.ru>
2020-04-28 13:36:25 +0300

    2. Перегрузка конструктора.[#265124] с изменением 1.

--- src/ru/job4j/animal/Predator.java
+++ src/ru/job4j/animal/Predator.java
@@ -2,6 +2,12 @@ package ru.job4j.animal;
 
 public class Predator extends Animal {
 
+
+    public Predator() {
+        super();
+        System.out.println("class Predator!!!");
+    }
+
     public Predator(String name) {
         super(name);
         System.out.println("class Predator!!!");
--- src/ru/job4j/animal/Tiger.java
+++ src/ru/job4j/animal/Tiger.java
@@ -2,6 +2,12 @@ package ru.job4j.animal;
 
 public class Tiger extends Predator {
 
+
+    public Tiger() {
+        super();
+        System.out.println("class Tiger!!!");
+    }
+
     public Tiger(String name) {
         super(name);
         System.out.println("class Tiger!!!");
