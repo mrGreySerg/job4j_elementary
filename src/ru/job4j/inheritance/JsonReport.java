d1d032d3a5281a89ee16c25726c41a9d1cfe8d0b
sergei kustrin <sergei@mail.ru>
2020-04-29 11:09:38 +0300

    4. Переопределение[#265125]

---
+++ src/ru/job4j/inheritance/JsonReport.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class JsonReport extends TextReport {
+
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "name:" + name + ","
+                + System.lineSeparator() + "body:" + body
+                + System.lineSeparator() + "}";
+    }
+
+    public static void main(String[] args) {
+        JsonReport report = new JsonReport();
+        System.out.println(report.generate("name", "body"));
+    }
+}
